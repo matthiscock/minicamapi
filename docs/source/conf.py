d2b4668cb43c88d421a30e50e13197b2fb19d029
matthiscock <matt@senses.co.uk>
2021-11-23 08:05:47 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Footprint - MiniCam API'
+copyright = '2021, Footprint Mapping Solutions Ltd'
+author = 'Matt Hiscock'
 
 release = '0.1'
 version = '0.1.0'
